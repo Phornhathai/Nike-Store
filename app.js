400af28ee6dc5dd1db58366678d5c993483b4912
Phornhathai <74613199+Phornhathai@users.noreply.github.com>
2023-06-25 14:06:26 +0700

    Update animation bar (translateX,transition)

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-console.log("Hollo this is js");
+const wrapper = document.querySelector(".sliderWrapper");
+
+// console.log(wrapper)
+// wrapper.style.backgroundColor = "red";
+// wrapper.style.transform = "translateX(-400vw)"
+
+const menuItems = document.querySelectorAll(".menuItem");
+
+menuItems.forEach((item,index)=>{
+    item.addEventListener("click", ()=>{
+        wrapper.style.transform = `translateX(${-100 * index}vw)`;
+    });
+});
